77ced32fa2ed247913c17392d0efbbc71b2de002
8Gates <cdsmith7@gmail.com>
2023-10-15 23:12:44 -0500

    added Google's static maps into trail cards

--- lib/lists/trail_list_view_builder.dart
+++ lib/lists/trail_list_view_builder.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:outline_app/map_widget.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TrailList extends StatelessWidget {
   final Future<List<Map<String, dynamic>>> trailsFuture;
@@ -22,15 +26,18 @@ class TrailList extends StatelessWidget {
             // shrinkWrap: true, // Set shrinkWrap to true
             itemCount: trails.length,
             itemBuilder: (BuildContext context, int index) {
+              final int id = trails[index]['id'];
               final String park = trails[index]['park'];
               final String name = trails[index]['name'];
               final double distance = trails[index]['distance'];
               final int elevationGain = trails[index]['elevation_gain'];
               final String difficulty = trails[index]['difficulty'];
               final String description = trails[index]['description'];
+              final double latitude = trails[index]['latitude'];
+              final double longitude = trails[index]['longitude'];
 
               // Exclude longitude, latitude, and description fields
-              return ExpandingCard(name, park, distance, elevationGain, difficulty, description);
+              return ExpandingCard(id, name, park, distance, elevationGain, difficulty, description, latitude, longitude);
             },
           );
         }
@@ -40,13 +47,18 @@ class TrailList extends StatelessWidget {
 }
 
 class ExpandingCard extends StatefulWidget {
+  final int id;
   final String name;
   final String park;
   final double distance;
   final int elevation;
   final String difficulty;
   final String description;
-  const ExpandingCard(this.name, this.park, this.distance, this.elevation, this.difficulty, this.description, {super.key});
+  final double latitude;
+  final double longitude;
+  final String mapKey = "API_KEY_HERE";
+
+  const ExpandingCard(this.id, this.name, this.park, this.distance, this.elevation, this.difficulty, this.description, this.latitude, this.longitude, {super.key});
 
   compressedContent(){
     return [ 
@@ -57,13 +69,19 @@ class ExpandingCard extends StatefulWidget {
     ];
   } 
 
-  expandedContent(){
+  expandedContent(int id, double latitude, double longitude){
     return [
       Text('Park: $park'),
       Text('Distance: $distance miles'),
       Text('Elevation Gain: $elevation ft'),
       Text('Difficulty: $difficulty'),
-      Text('Description: $description')
+      Text('Description: $description'),
+      id == 1 && mapKey != "API_KEY_HERE" ? Center(child: 
+        Image.network(
+          'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=10&size=400x400&markers=$latitude,$longitude&key=$mapKey',
+        )
+      )
+ : const Text('')
     ];
   }
 
@@ -88,7 +106,7 @@ class _ExpandingCardState extends State<ExpandingCard> {
         title: Text('$name Trail'),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: isExpanded ? widget.expandedContent() : widget.compressedContent()
+          children: isExpanded ? widget.expandedContent(widget.id, widget.latitude, widget.longitude) : widget.compressedContent()
         ),
         trailing: IconButton(
           icon: Icon(isExpanded ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down),
